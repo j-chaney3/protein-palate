9266babb20b3ea45ef9798eee896fe12ecb425a0
Jame Chaney <macneilslt@gmail.com>
2023-12-15 13:27:47 -0500

    test backend configuration

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+
+const { test } = require('../controllers/auhtController');
+
+//middleware
+router.use(
+	cors({
+		credentials: true,
+		origin: 'http:http://localhost:3000',
+	})
+);
+
+//routes imported from controllers
+router.get('/', test);
+
+module.exports = router;
