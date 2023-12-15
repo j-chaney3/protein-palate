28c8d0548caefd4a6b29f4f7b9c6d1eb49a88cca
Jame Chaney <macneilslt@gmail.com>
2023-12-15 13:09:17 -0500

    basic login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Login() {
-	return <div>Login</div>;
+	const [data, setData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const loginUser = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<div>
+			<form onSubmit={loginUser}>
+				<label className="px-1">Email:</label>
+				<input
+					className="border rounded px-1"
+					type="email"
+					placeholder="email"
+					value={data.email}
+					onChange={(e) => setData({ ...data, email: e.target.value })}
+				/>
+				<label className="px-1">Password:</label>
+				<input
+					className="border rounded px-1"
+					type="password"
+					placeholder="password"
+					value={data.password}
+					onChange={(e) => setData({ ...data, password: e.target.value })}
+				/>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 mx-2 rounded text-m"
+					type="submit"
+				>
+					Login
+				</button>
+			</form>
+		</div>
+	);
 }
